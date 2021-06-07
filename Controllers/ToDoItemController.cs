64e1933bd52db0eb17b0f137072a35ebb3b644bd
Jagrati Modi <jagratimodi@yahoo.in>
2021-06-07 11:19:53 +0530

    Adding Web API Project

---
+++ Controllers/ToDoItemController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoAPI.Models;
+using ToDoAPI.Services;
+
+namespace ToDoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoItemController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<ToDoItem>> GetAll() => ToDoItemService.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<ToDoItem> Get(int id)
+        {
+            var todoItem = ToDoItemService.Get(id);
+            if (todoItem == null)
+                return NotFound();
+            return todoItem;
+        }
+
+        [HttpPost]
+        public IActionResult Create(ToDoItem item)
+        {
+            ToDoItemService.Add(item);
+            return CreatedAtAction(nameof(Create), new { Id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, ToDoItem item)
+        {
+            if (id != item.Id)
+                return BadRequest();
+            var existingItem = ToDoItemService.Get(id);
+            if (existingItem is null)
+                return NotFound();
+            ToDoItemService.Update(item);
+            return NoContent();
+        }
+
+        // DELETE api/<ToDoItemController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var toDoItem = ToDoItemService.Get(id);
+
+            if (toDoItem is null)
+                return NotFound();
+
+            ToDoItemService.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Services/ToDoItemService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Services
+{
+    public class ToDoItemService
+    {
+        static List<ToDoItem> ToDoItems;
+
+        static int nextId = 3;
+
+        static ToDoItemService()
+        {
+            ToDoItems = new List<ToDoItem>
+            {
+                new ToDoItem {Id= 1, Name="Start session", IsCompleted= false},
+                new ToDoItem {Id= 2, Name="Start introduction", IsCompleted= false}
+            };
+        }
+
+        public static List<ToDoItem> GetAll() => ToDoItems;
+
+        public static ToDoItem Get(int id) => ToDoItems.FirstOrDefault(i => i.Id == id);
+
+        public static void Add(ToDoItem item)
+        {
+            item.Id = nextId++;
+            ToDoItems.Add(item);
+        }
+
+        public static void Update(ToDoItem item)
+        {
+            var index = ToDoItems.FindIndex(i => i.Id == item.Id);
+            if (index == -1)
+                return;
+
+            ToDoItems[index] = item;
+        }
+
+        public static void Delete(int id)
+        {
+            //ToDoItems.Add(item);
+            var item = Get(id);
+            if (item is null)
+                return;
+
+            ToDoItems.Remove(item);
+        }
+    }
+}
